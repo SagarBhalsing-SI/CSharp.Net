5cc566c1d8df36425e1408e188bba035cd58965a
SagarBhalsing-SI <140367558+SagarBhalsing-SI@users.noreply.github.com>
2023-09-11 11:25:19 +0530

    Update Program.cs

--- Match_Details_Management_System/Program.cs
+++ Match_Details_Management_System/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Match_Details_Management_System
+namespace Match_Details_Management_System
 {
     internal class Program
     {
@@ -6,7 +6,6 @@
         static void Main(string[] args)
         {
             MatchManagement matchManagement = new MatchManagement();
-            //SwitchInterface si = new SwitchInterface();
             UserInterface ui = new UserInterface();
             BigInterface bigInterface = new BigInterface(); 
             string c;
@@ -15,6 +14,7 @@
                 Console.WriteLine("===============================Sports Management System===================================");
                 Console.WriteLine("==========================================================================================");
                 Console.WriteLine("1. Display Matches\n" +
+                                  "2. Add Match\n"+
                                   "2. Display Matche By Id\n" +
                                   "3. Update Score By Id\n" +
                                   "4. Remove Match By Id\n" +
@@ -29,28 +29,34 @@
                             matchManagement.DisplayMatches();
                             break;
                         }
+
                     case 2:
+                        {
+                            matchManagement.AddMatch();
+                            break;
+                        }
+                    case 3:
                         {
                             matchManagement.GetMatchById();
                             break;
                         }
 
-                    case 3:
+                    case 4:
                         {
                             matchManagement.UpdateMatchScoreById();
                             break;
                         }
-                    case 4:
+                    case 5:
                         {
                             matchManagement.RemoveMatchById();
                             break;
                         }
-                    case 5:
+                    case 6:
                          {
                             bigInterface.BigInterfaceui(); 
                          break;
                         }
-                    case 6:
+                    case 7:
                         {
 
                           break;
@@ -65,63 +71,3 @@
     }
 
     }
-
-
-/*   do
-            {
-            Console.WriteLine("===============================Sports Management System===================================");
-            Console.WriteLine("==========================================================================================");
-            Console.WriteLine("1. Display Matches\n" +
-                              "2. Display Matche By Id\n" +
-                              "3. Update Score By Id\n" +
-                              "4. Remove Match By Id\n" +
-                              "5. Extra Feature\n" +
-                              "6. Exit\n");
-
-            int choice = Convert.ToInt32(Console.ReadLine());
-             switch (choice)
-             {
-                 case 1:
-                     {
-                         matchManagement.DisplayMatches();
-                         break;
-                     }
-                 case 2:
-                     {
-                         matchManagement.GetMatchById();
-                         break;
-                     }
-
-                 case 3:
-                     {
-                         matchManagement.UpdateMatchScoreById();
-                         break;
-                     }
-                 case 4:
-                     {
-                         matchManagement.RemoveMatchById();
-                         break;
-                     }
-                // case 5:
-                  //   {
-                    //     ui.DisplayFeatureInterface();
-                      //   break;
-                     //}
-                 //case 6:
-                     //{
-
-                       //  break;
-                     //}
-             }
-             Console.WriteLine("\n");
-            Console.WriteLine("Do you want to continue with Matches Management System Menu Y/N:");
-            c = Console.ReadLine();
-            }
-            while (c == "Y");
-             }
-*/
-
-
-
-
-
